1878e9bc495a740578590952b618337ca53ed686
M-Asyfa <muhammad.asyfa.1995@gmail.com>
2025-06-08 12:04:24 +0700

    Modifying cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tonjoo-test2-frontend.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
